7a25526cca0e16779e3bdc49dc5a83c1e5c4731b
anthonyjlmorel <anthony.jlmorel@gmail.com>
2016-04-28 14:15:47 +0200

    Bug fix (duplicated fighters)

--- js/p-scripts/results.js
+++ js/p-scripts/results.js
@@ -80,7 +80,7 @@ var resultsReady = function(){
 			for(var j in resultsLst[i].results.individualResults){
 				
 				resultsLst[i].results.individualResults[j].forEach(function(fighter){
-					var key = fighter.lastName + "_" + fighter.firstName;
+					var key = fighter.lastName.trim() + "_" + fighter.firstName.trim();
 					if(!fighterMap[key]){
 						fighterMap[key] = {
 							firstName: fighter.firstName,
@@ -105,7 +105,7 @@ var resultsReady = function(){
 				resultsLst[i].results.teamResults[j].forEach(function(re){
 					
 					re.fighters.forEach(function(f){
-						var key = f.lastName + "_" + f.firstName;
+						var key = f.lastName.trim() + "_" + f.firstName.trim();
 						if(!fighterMap[key]){
 							fighterMap[key] = {
 								firstName: f.firstName,
