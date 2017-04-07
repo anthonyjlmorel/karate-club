ae676d41eb5339e77f36f946ff3d28c80469f92c
anthonyjlmorel <anthony.jlmorel@gmail.com>
2017-04-07 14:33:24 +0200

    updates

--- pages/w.php
+++ pages/w.php
@@ -5,6 +5,7 @@
 		<div class="intro-lead-in">École de</div>
 		<div class="intro-heading">Karaté Shotokan</div>
 		<a href="<?php echo SITE_WEB_ADDR.'/?p=club'; ?>" class="page-scroll btn btn-xl">Entrer</a>
+		<div class="intro-lead-in">Krav Maga et Self Défense</div>
 	</div>
 </div>
 
